cb2e1b1d5bea954e65cbc3612813ff2c23bdfafe
lizhichange@163.com <lizhi1234>
2019-03-08 16:41:42 +0800

    fix

--- db/orm.go
+++ db/orm.go
@@ -1,11 +1,18 @@
 package db
 
 import (
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 ) // import your used driver
 
 func init() {
 	_ = orm.RegisterDriver("mysql", orm.DRMySQL)
-	_ = orm.RegisterDataBase("default", "mysql", "root:root@tcp(127.0.0.1:3306)/test?charset=utf8", 30)
+
+	user := beego.AppConfig.String("mysqluser")
+	pass := beego.AppConfig.String("mysqlpass")
+	url := beego.AppConfig.String("mysqlurls")
+	db := beego.AppConfig.String("mysqldb")
+
+	_ = orm.RegisterDataBase("default", "mysql", ""+user+":"+pass+"@tcp("+url+":3306)/"+db+"?charset=utf8", 30)
 }
