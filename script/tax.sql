34c741dbde19a40fba9fbde19d5fc2ac12bae050
Your Name <lizhichange@163.com>
2020-03-13 16:09:05 +0800

    fix

--- script/tax.sql
+++ script/tax.sql
@@ -17,6 +17,9 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
+create database  tax default character set utf8mb4 collate utf8mb4_unicode_ci;
+
+USE tax;
 -- ----------------------------
 -- Table structure for insurance_info
 -- ----------------------------
